ac4b3de80c287d7ffa610477507efe7ee0a00508
Maeka Shiroyama <shiroyama.maeka@gmail.com>
2018-09-18 10:43:47 +0000

    init

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,65 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  def index
+     @blogs = Blog.where(user_id:current_user.id) 
+
+  end
+
+
+  def new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
+      @blog = Blog.new
+    end
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
+    if @blog.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to controller: 'users', action: 'show', id:current_user.id, notice: "ブログを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render 'new'
+    end
+  end
+  
+  def confirm
+    @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
+    render :new if @blog.invalid?
+  end
+  
+  def show
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
+
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @blog.update(blog_params)
+      redirect_to controller: 'users', action: 'show', id:current_user.id, notice: "ブログを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to controller: 'users', action: 'show', id:current_user.id, notice:"ブログを削除しました！"
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content, :user_id)
+  end
+  
+  def set_blog
+      @blog = Blog.find(params[:id])
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  get "/users/favorites/:id" => "users#favorites"
+
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+  resources :tops
+  resources :favorites, only: [:create, :destroy]
+end
