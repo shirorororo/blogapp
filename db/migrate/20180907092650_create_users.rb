9957b1b69ef2b1a770fdeae998b97b5f6924a4df
Maeka Shiroyama <shiroyama.maeka@gmail.com>
2018-09-18 14:44:31 +0000

    change migration

--- db/migrate/20180907092650_create_users.rb
+++ db/migrate/20180907092650_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :password_digest
       t.datetime :created_at, null: false
       t.datetime :updated_at, null: false
-      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index [:email], name:index_users_on_email, unique: true
   
       t.timestamps
     end
