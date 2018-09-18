4bdd471b68dbaf91928d44582b72411ec5482008
Maeka Shiroyama <shiroyama.maeka@gmail.com>
2018-09-18 15:04:53 +0000

    change migration

--- db/migrate/20180906131425_create_blogs.rb
+++ db/migrate/20180906131425_create_blogs.rb
@@ -1,10 +1,10 @@
 class CreateBlogs < ActiveRecord::Migration[5.1]
   def change
-    create_table :blogs do |t|
-       t.string :title
-       t.text :content
-       t.bigint :user_id
-       t.index [:user_id], name:index_blogs_on_user_id
+    create_table "blogs" do |t|
+       t.string "title"
+       t.text "content"
+       t.bigint "user_id"
+       t.index ["user_id"], name: "index_blogs_on_user_id"
  
        t.timestamps
       
--- db/migrate/20180910023923_create_favorites.rb
+++ db/migrate/20180910023923_create_favorites.rb
@@ -1,12 +1,10 @@
 class CreateFavorites < ActiveRecord::Migration[5.1]
   def change
-    create_table :favorites do |t|
-      t.integer :user_id
-      t.integer :blog_id
-      t.integer :user_id
-      t.integer :blog_id
-      t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false
+    create_table "favorites" do |t|
+      t.integer "user_id"
+      t.integer "blog_id"
+      t.datetime "created_at", null "false"
+      t.datetime "updated_at", null "false"
       t.timestamps
     end
   end
